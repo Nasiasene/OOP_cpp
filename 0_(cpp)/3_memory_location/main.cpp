7a59395135f00cacdda7b76291dd902b541c7828
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-09-21 12:26:57 -0300

    memory location, structs and enums in cpp

--- 0_(cpp)/3_memory_location/main.cpp
+++ 0_(cpp)/3_memory_location/main.cpp
@@ -11,6 +11,7 @@ or: using str = string;
 */
 
 int main(){
+       //Strings
     str hello = "testing!", test_string;
 
     cout << "Insert an full name: ";
@@ -19,10 +20,45 @@ int main(){
            it's necessary to use getline(std::cin >> std::ws, <name_var>).
     */
 
-   test_string += hello;  // same as test_string.append(str);
+   test_string = test_string + " " + hello;  // same as test_string.append(str);
    cout << test_string;
 
 
+   //Dynamic Memory:
+   string *pNames = nullptr;
+   int size;
+   cout << "\n\nPut the size of the array: ";
+   cin >> size;
 
+   pNames = new string[size];
 
+   for (int i=0; i < size; i++){
+       string new_str;
+       cout << "Fill the array with strings: ";
+       cin >> new_str;
+
+       pNames[i] = new_str;
+   }
+
+   for ( int i = 0; i < size; i++){
+       cout << pNames[i] << endl;
+   }
+
+   delete[] pNames;
+   cout << endl << pNames[0] << endl;   //will be an randon value.
+
+   int *pInt = nullptr;
+   pInt = new int;
+
+   *pInt = 22;           //Remember, when you work with pointers, his actual value is an memory position.
+
+   cout << "Pointer memory: " << pInt << "\nPointer value: " << *pInt << endl;
+
+   delete pInt;
+    
+   cout << "Pointer memory: " << pInt << "\nPointer value: " << *pInt << endl;
+
+   
+
+   
 };
