264e08b7fbdec2aa196e3e9c6c8a981762f085b2
Davi Nasiasene Amorim <davi.ted@hotmail.com>
2023-11-07 12:13:14 -0300

    test 3

--- tasks/tests/third_test/A/main.cpp
+++ tasks/tests/third_test/A/main.cpp
@@ -1,22 +1,362 @@
+// uso do copilot: GETS/SETS
+// Uso do GPT: main
+
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <stdexcept>
 
 using namespace std;
 
+class Calcado; //Para poder usar na classe fornecedor.
+
+// 1. **fornecedor**
+//    - Atributos: nome (string), cnpj (string).
+//    - Métodos: construtor, getters e setters, listar produtos do fornecedor.
+class Fornecedor{
+    string nome, cnpj;
+    
+    public:
+        Fornecedor(string no, string cn) : nome{no}, cnpj{cn} {}
+
+        //GETS:
+        string get_nome(){return nome;}
+        string get_cnpj(){return cnpj;}
+
+        //SETS:
+        void set_nome(string no){nome = no;}
+        void set_cnpj(string cn){cnpj = cn;}
+
+        // // //LISTAR PRODUTOS DO FORNECEDOR:
+        // void listar_produtos(vector<Calcado>& calcados) {
+        //     for (auto& calcado : calcados) {
+        //         if (calcado.get_fornecedor() == this) {
+        //             cout << "Marca: " << calcado.get_marca() << ", Modelo: " << calcado.get_modelo() << endl;
+        //         }
+        //     }
+        // }
+};
+
+// 2. **calcado** (classe base, abstrata)
+//    - Atributos: marca (string), modelo (string), cor (string), tamanho (int),
+//      preço (double), fornecedor (ponteiro para fornecedor).
+//    - Métodos: construtor, getters e setters, exibir informações (virtual)
+// Tratamento de exceções: faça com que só produtos com preços positivos possam 
+// ser incluídos no estoque - use o tratamento de exceções para garantir isso.
+class Calcado{
+    private:
+        string marca, modelo, cor;
+        int tamanho;
+        double preco;
+        Fornecedor *fornecedor;
+
+    public:
+        Calcado(string ma, string mo, string co, int ta, double pr, Fornecedor &f) : marca{ma}, modelo{mo}, cor{co}, tamanho{ta}, preco{pr}, fornecedor{&f} {}
+
+        //GETS:
+        string get_marca(){return marca;}
+        string get_modelo(){return modelo;}
+        string get_cor(){return cor;}
+        int get_tamanho(){return tamanho;}
+        double get_preco(){return preco;}
+        Fornecedor *get_fornecedor(){return fornecedor;}
+
+        //SETS:
+        void set_marca(string ma){marca = ma;}
+        void set_modelo(string mo){modelo = mo;}
+        void set_cor(string co){cor = co;}
+        void set_tamanho(int ta){tamanho = ta;}
+        void set_preco(double pr) {
+            if (pr <= 0) {
+                preco = pr;
+            } else {
+                throw invalid_argument("Preço nao pode ser negativo."); //CUIDANDO DAS EXCEÇÕES EM CASO DE NUMEROS NEGATIVOS.
+            }
+        }
+        void set_fornecedor(Fornecedor &f){fornecedor = &f;}
+
+        //EXIBIR INFORMAÇÕES:
+        virtual void exibir_informacoes(){ //Método virtual, tornando a classe abstrata.
+            cout << "Marca: " << get_marca() << endl;
+            cout << "Modelo: " << get_modelo() << endl;
+            cout << "Cor: " << get_cor() << endl;
+            cout << "Tamanho: " << get_tamanho() << endl;
+            cout << "Preço: " << get_preco() << endl;
+            cout << "Fornecedor: " << get_fornecedor()->get_nome() << endl;
+        }
+
+        void listar_produtos(vector<Calcado>& calcados) {
+            for (auto& calcado : calcados) {
+                if (calcado.get_fornecedor() == fornecedor) {
+                    cout << "Marca: " << calcado.get_marca() << ", Modelo: " << calcado.get_modelo() << endl;
+                }
+            }
+        }
+};
+
+// 3. **tenis** (herança pública de calcado)
+//    - Atributos: tipo de esporte (string).
+//    - Métodos: construtor, getters e setters, sobrescrever o método de exibir 
+//      informações.
+class Tenis : public Calcado{
+    string tipo_esporte;
+
+    public:
+        Tenis(string ma, string mo, string co, int ta, double pr, Fornecedor &f, string te) : Calcado(ma, mo, co, ta, pr, f), tipo_esporte{te} {}
+
+        //GETS:
+        string get_tipo_esporte(){return tipo_esporte;}
+
+        //SETS:
+        void set_tipo_esporte(string te){tipo_esporte = te;}
+
+        //SOBRESCREVER O MÉTODO DE EXIBIR INFORMAÇÕES:
+        virtual void exibir_informacoes() override{
+            Calcado::exibir_informacoes();
+            cout << "Tipo de esporte: " << get_tipo_esporte() << endl;
+        }
+
+};
+
+// 4. **sapato_social** (herança pública de calcado)
+//    - Atributos: material (string).
+//    - Métodos: construtor, getters e setters, sobrescrever o método de exibir 
+//      informações.
+class SapatoSocial : public Calcado{
+    string material;
+
+    public:
+        SapatoSocial(string ma, string mo, string co, int ta, double pr, Fornecedor &f, string mat) : Calcado(ma, mo, co, ta, pr, f), material{mat} {}
+
+        //GETS:
+        string get_material(){return material;}
+
+        //SETS:
+        void set_material(string mat){material = mat;}
+
+        //SOBRESCREVER O MÉTODO DE EXIBIR INFORMAÇÕES:
+        virtual void exibir_informacoes() override{
+            Calcado::exibir_informacoes();
+            cout << "Material: " << get_material() << endl;
+        }
+};
+
+// 5. **estoque**
+//    - Atributos: lista de produtos em estoque (vector de calcado).
+//    - Métodos: construtor, destrutor, adicionar e remover produtos ao/do estoque,
+//      calcular o valor total do estoque, encontrar produtos no estoque.
+class Estoque{
+    vector<Calcado> produtos;
+
+    public:
+        Estoque() {}
+        ~Estoque() {produtos.clear();}
+
+        //ADICIONAR E REMOVER PRODUTOS AO/DO ESTOQUE:
+        void adicionar_produto(Calcado &c){produtos.push_back(c);}
+
+        void remover_produto(const string& marca, const string& modelo) {
+            for (auto it = produtos.begin(); it != produtos.end(); ++it) {
+                if (it->get_marca() == marca && it->get_modelo() == modelo) {
+                    produtos.erase(it);
+                    break;
+                }
+            }
+        }
+
+
+        //CALCULAR O VALOR TOTAL DO ESTOQUE:
+        double calcular_valor_total(){
+            double valor_total = 0;
+            for(auto &calcado : produtos){
+                valor_total += calcado.get_preco();
+            }
+            return valor_total;
+        }
+
+        //ENCONTRAR PRODUTOS NO ESTOQUE:
+        void encontrar_produtos(string ma, string mo){
+            for(auto &calcado : produtos){
+                if(calcado.get_marca() == ma && calcado.get_modelo() == mo){
+                    calcado.exibir_informacoes();
+                }
+            }
+        }
+};
+
+// 6. **fabrica** (singleton)
+//    - Atributos: fornecedores (map de fornecedor, string como chave), estoque 
+//      (instância de estoque).
+//    - Métodos: construtor, destrutor, adicionar e remover fornecedor, adicionar
+//      e remover produto ao/do estoque, calcular o valor total do estoque, 
+//      encontrar produtos no estoque.
+class Fabrica{
+Fabrica() {} //Construtor privado para o singleton.
+static Fabrica *_instance;
+
+    public:
+    map<string, Fornecedor> fornecedores;
+    Estoque estoque;
+    ~Fabrica() {fornecedores.clear();}
+
+    static Fabrica *get_instance(){
+        if (_instance == NULL)
+            _instance = new Fabrica();
+        return _instance;
+    }
+
+    //ADICIONAR E REMOVER FORNECEDOR:
+    void adicionar_fornecedor(Fornecedor &f){fornecedores.insert(pair<string, Fornecedor>(f.get_nome(), f));}
+    void remover_fornecedor(Fornecedor &f){fornecedores.erase(f.get_nome());}
+
+    //ADICIONAR E REMOVER PRODUTO AO/DO ESTOQUE:
+    void adicionar_produto(Calcado &c){estoque.adicionar_produto(c);}
+    void remover_produto(Calcado &c){estoque.remover_produto(c.get_marca(), c.get_modelo());}
+    //CALCULAR O VALOR TOTAL DO ESTOQUE:
+    double calcular_valor_total(){return estoque.calcular_valor_total();}
+    //ENCONTRAR PRODUTOS NO ESTOQUE:
+    void encontrar_produtos(string ma, string mo){estoque.encontrar_produtos(ma, mo);}
+};
+Fabrica *Fabrica::_instance = NULL; //Inicializando o singleton.
 
 
 
+// Uso das classes: implemente um sistema com um menu que permita: cadastrar, 
+// remover e listar fornecedores; cadastrar, remover e listar tênis e sapatos
+// sociais no estoque; exibir valor total do estoque; buscar produtos no estoque;
+int main() {
+    Fabrica *fabrica = Fabrica::get_instance(); //Singleton
 
+    while (true) {
+        int opcao;
+        cout << "Escolha uma opção:" << endl;
+        cout << "1. Cadastrar fornecedor" << endl;
+        cout << "2. Remover fornecedor" << endl;
+        cout << "3. Listar fornecedores" << endl;
+        cout << "4. Cadastrar tênis" << endl;
+        cout << "5. Cadastrar sapato social" << endl;
+        cout << "6. Remover produto do estoque" << endl;
+        cout << "7. Exibir valor total do estoque" << endl;
+        cout << "8. Buscar produtos no estoque" << endl;
+        cout << "0. Sair" << endl;
+        cout << "Opcao: ";
+        cin >> opcao;
 
-
-
-
-
-
-
-
-
-int main(){
-
+        switch (opcao) {
+            case 1: {
+                string nome, cnpj;
+                cout << "Nome do fornecedor: ";
+                cin.ignore();
+                getline(cin, nome);
+                cout << "CNPJ do fornecedor: ";
+                cin >> cnpj;
+                Fornecedor fornecedor(nome, cnpj);
+                fabrica->adicionar_fornecedor(fornecedor);
+                cout << "Fornecedor cadastrado com sucesso." << endl;
+                break;
+            }
+            case 2: {
+                string nome;
+                cout << "Nome do fornecedor a ser removido: ";
+                cin.ignore();
+                getline(cin, nome);
+                auto it = fabrica->fornecedores.find(nome);
+                if (it != fabrica->fornecedores.end()) {
+                    fabrica->remover_fornecedor(it->second);
+                    cout << "Fornecedor removido com sucesso." << endl;
+                } else {
+                    cout << "Fornecedor não encontrado." << endl;
+                }
+                break;
+            }
+            case 3: {
+                cout << "Lista de fornecedores:" << endl;
+                for (auto &pair : fabrica->fornecedores) {
+                    cout << "Nome: " << pair.first << ", CNPJ: " << pair.second.get_cnpj() << endl;
+                }
+                break;
+            }
+            case 4: {
+                string ma, mo, cor, tipo;
+                int tamanho;
+                double preco;
+                cout << "Marca: ";
+                cin.ignore();
+                getline(cin, ma);
+                cout << "Modelo: ";
+                getline(cin, mo);
+                cout << "Cor: ";
+                getline(cin, cor);
+                cout << "Tamanho: ";
+                cin >> tamanho;
+                cout << "Preço: ";
+                cin >> preco;
+                cout << "Tipo de esporte: ";
+                cin.ignore();
+                getline(cin, tipo);
+                Fornecedor fornecedor("", "");  // Fornecedor vazio temporário
+                Tenis tenis(ma, mo, cor, tamanho, preco, fornecedor, tipo);
+                fabrica->estoque.adicionar_produto(tenis);
+                cout << "Tenis cadastrado com sucesso." << endl;
+                break;
+            }
+            case 5: {
+                string ma, mo, cor, material;
+                int tamanho;
+                double preco;
+                cout << "Marca: ";
+                cin.ignore();
+                getline(cin, ma);
+                cout << "Modelo: ";
+                getline(cin, mo);
+                cout << "Cor: ";
+                getline(cin, cor);
+                cout << "Tamanho: ";
+                cin >> tamanho;
+                cout << "Preco: ";
+                cin >> preco;
+                cout << "Material: ";
+                cin.ignore();
+                getline(cin, material);
+                Fornecedor fornecedor("", "");  // Fornecedor vazio temporário
+                SapatoSocial sapato_social(ma, mo, cor, tamanho, preco, fornecedor, material);
+                fabrica->estoque.adicionar_produto(sapato_social);
+                cout << "Sapato social cadastrado com sucesso." << endl;
+                break;
+            }
+            case 6: {
+                string ma, mo;
+                cout << "Marca do produto a ser removido: ";
+                cin.ignore();
+                getline(cin, ma);
+                cout << "Modelo do produto a ser removido: ";
+                getline(cin, mo);
+                fabrica->estoque.remover_produto(ma, mo);
+                cout << "Produto removido do estoque." << endl;
+                break;
+            }
+            case 7: {
+                double valor_total = fabrica->estoque.calcular_valor_total();
+                cout << "Valor total do estoque: " << valor_total << endl;
+                break;
+            }
+            case 8: {
+                string ma, mo;
+                cout << "Marca do produto a ser buscado: ";
+                cin.ignore();
+                getline(cin, ma);
+                cout << "Modelo do produto a ser buscado: ";
+                getline(cin, mo);
+                fabrica->estoque.encontrar_produtos(ma, mo);
+                break;
+            }
+            case 0: {
+                delete fabrica;
+                return 0;
+            }
+            default:
+                cout << "Opção inalida. Tente novamente." << endl;
+        }
+    }
 }
--- tasks/tests/third_test/B/main.cpp
+++ tasks/tests/third_test/B/main.cpp
@@ -4,19 +4,315 @@
 
 using namespace std;
 
+// 1. **uva**
+//    - Atributos: nome (string), safra (int), preço por quilo (double).
+//    - Métodos: construtor, getters e setters.
+class Uva{
+    string nome;
+    int safra;
+    double preco_por_quilo;
+
+    public:
+        Uva(string nn, int ss, double pq): nome(nn), safra(ss), preco_por_quilo(pq){}
+
+        //GETTERS
+        string get_nome(){return nome;}
+        int get_safra(){return safra;}
+        double get_preco_por_quilo(){return preco_por_quilo;}
+
+        //SETTERS
+        void set_nome(string nn){nome = nn;}
+        void set_safra(int ss){safra = ss;}
+        void set_preco_por_quilo(double pq){preco_por_quilo = pq;}
+};
+ 
+// 2. **vinho** (classe base, abstrata)
+//    - Atributos: nome (string), safra (int), volume (double), uva (ponteiro para
+//      a classe Uva), quantidade (int, variável de classe)
+//    - Métodos: construtor, getters e setters, exibir informações (virtual), 
+//      get_quantidade (método de classe)
+// Tratamento de exceções: use o tratamento de exceções para garantir que não
+// sejam cadastrados vinhos sem uva definida ou ano de safra inválido.
+ class Vinho{
+        protected:
+        string nome;
+        int safra;
+        double volume;
+        Uva *uva;
+        int quantidade;
+    
+        public:
+            Vinho(string nn, int ss, double vv, Uva *uu, int qq){
+                //Tratamento de exceções.
+                if (uu == NULL)
+                    throw "Uva não definida";
+                if (ss < 0)
+                    throw "Safra inválida"; 
+                nome = nn;
+                safra = ss;
+                volume = vv;
+                uva = uu;
+                quantidade = qq;
+            }
+    
+            //GETTERS
+            string get_nome(){return nome;}
+            int get_safra(){return safra;}
+            double get_volume(){return volume;}
+            Uva *get_uva(){return uva;}
+            int get_quantidade() { return quantidade; }
+    
+            //SETTERS
+            void set_nome(string nn){nome = nn;}
+            void set_safra(int ss){safra = ss;}
+            void set_volume(double vv){volume = vv;}
+            void set_uva(Uva *uu){uva = uu;}
+            void set_quantidade(int qq){quantidade = qq;}
+    
+            virtual void exibir_informacoes(){
+                cout << "Nome: " << nome << endl;
+                cout << "Safra: " << safra << endl;
+                cout << "Volume: " << volume << endl;
+                cout << "Uva: " << uva->get_nome() << endl;
+                cout << "Quantidade: " << quantidade << endl;
+            }
+ };
 
 
+// 3. **vinho_tinto** (herança pública de vinho)
+//    - Atributos: nível de tanino (float), quantidade de polifenois (float)
+//    - Métodos: construtor, getters e setters, sobrescrever exibir informações
+class VinhoTinto : public Vinho{
+    double nivel_de_tanino;
+    double quantidade_de_polifenois;
+
+    public:
+        VinhoTinto(string nn, int ss, double vv, Uva *uu, int qq, double nt, double qp): Vinho(nn, ss, vv, uu, qq), nivel_de_tanino(nt), quantidade_de_polifenois(qp){}
+
+        //GETTERS
+        double get_nivel_de_tanino(){return nivel_de_tanino;}
+        double get_quantidade_de_polifenois(){return quantidade_de_polifenois;}
+
+        //SETTERS
+        void set_nivel_de_tanino(double nivel_de_tanino){nivel_de_tanino = nivel_de_tanino;}
+        void set_quantidade_de_polifenois(double quantidade_de_polifenois){quantidade_de_polifenois = quantidade_de_polifenois;}
+
+        void exibir_informacoes(){
+            Vinho::exibir_informacoes();
+            cout << "Nível de tanino: " << nivel_de_tanino << endl;
+            cout << "Quantidade de polifenois: " << quantidade_de_polifenois << endl;
+        }
+};
+ 
+// 4. **vinho_branco** (herança pública de Vinho)
+//    - Atributos: subtipo (string, ex. "Seco" ou "Suave")
+//    - Métodos: construtor, getters e setters, sobrescrever exibir informações
+class VinhoBranco : public Vinho{
+    string subtipo;
+
+    public:
+        VinhoBranco(string nn, int ss, double vv, Uva *uu, int qq, string st): Vinho(nn, ss, vv, uu, qq), subtipo(st){}
+
+        //GETTERS
+        string get_subtipo(){return subtipo;}
+
+        //SETTERS
+        void set_subtipo(string subtipo){subtipo = subtipo;}
+
+        void exibir_informacoes(){
+            Vinho::exibir_informacoes();
+            cout << "Subtipo: " << subtipo << endl;
+        }
+};
 
 
+// 5. **cliente**
+//    - Atributos: nome (string), sobrenome (string), email (string), telefone 
+//    (string).
+//    - Métodos: construtor, getters e setters.
+class Client{
+    string nome;
+    string sobrenome;
+    string email;
+    string telefone;
 
+    public:
+        Client(string nn, string sn, string em, string tel): nome(nn), sobrenome(sn), email(em), telefone(tel){}
 
+        //GETTERS
+        string get_nome(){return nome;}
+        string get_sobrenome(){return sobrenome;}
+        string get_email(){return email;}
+        string get_telefone(){return telefone;}
 
+        //SETTERS
+        void set_nome(string nn){nome = nn;}
+        void set_sobrenome(string sn){sobrenome = sn;}
+        void set_email(string em){email = em;}
+        void set_telefone(string tel){telefone = tel;}
+};
+ 
+// 6. **venda**
+//    - Atributos: cliente (ponteiro para a classe cliente), vinho (ponteiro para
+//      a classe vinho), quantidade (int).
+//    - Métodos: construtor, getters e setters, calcular_total() para calcular o 
+//      valor total da venda.
+class Venda{
+    Client *cliente;
+    Vinho *vinho;
+    int quantidade;
 
+    public:
+        Venda(Client *cc, Vinho *vv, int qq): cliente(cc), vinho(vv), quantidade(qq){}
 
+        //GETTERS
+        Client *get_cliente(){return cliente;}
+        Vinho *get_vinho(){return vinho;}
+        int get_quantidade(){return quantidade;}
 
+        //SETTERS
+        void set_cliente(Client *cc){cliente = cc;}
+        void set_vinho(Vinho *vv){vinho = vv;}
+        void set_quantidade(int qq){quantidade = qq;}
 
+        double calcular_total(){
+            return quantidade * vinho->get_volume();
+        }
+};
+ 
+// 7. **vinicola** (singleton)
+//    - Atributos: lista de uvas (vector de uva), lista de vinhos (vector de
+//      vinho), lista de clientes (vector de cliente), lista de vendas (vector 
+//      de venda).
+//    - Métodos: adicionar uva, adicionar vinho, adicionar cliente, registrar 
+//      venda, calcular soma de todas as vendas.
+class Vinicola{
+    Vinicola () {}
+    static Vinicola *_instance; //ponteiro para a instância única da classe.
 
+    protected:
+        vector<Uva> lista_de_uvas;
+        vector<Vinho> lista_de_vinhos;
+        vector<Client> lista_de_clientes;
+        vector<Venda> lista_de_vendas;
 
-int main(){
+    public:
+        static Vinicola *get_instance(){ //método para acessar a instância única da classe.
+            if (_instance == NULL)
+                _instance = new Vinicola();
+            return _instance;
+        }
 
+        void adicionar_uva(Uva uu){lista_de_uvas.push_back(uu);}
+        void adicionar_vinho(Vinho vv){lista_de_vinhos.push_back(vv);}
+        void adicionar_cliente(Client cc){lista_de_clientes.push_back(cc);}
+
+        void registrar_venda(Venda vv){lista_de_vendas.push_back(vv);}
+
+        double calcular_soma_de_todas_as_vendas(){
+            double soma = 0;
+            for (auto i = 0; i < int(lista_de_vendas.size()); i++){
+                soma += lista_de_vendas[i].calcular_total();
+            }
+            return soma;
+        }
+};
+Vinicola *Vinicola::_instance = NULL; //inicialização do ponteiro para a instância única da classe.
+
+ 
+// 8. **vinoteca** (classe genérica usando templates)
+//    - Atributos: lista de vinhos (vector de T), onde T é o tipo de vinho (pode 
+//      ser vinho_tinto, vinho_branco, etc.).
+//    - Métodos: adicionar vinho, listar vinhos, encontrar vinho por safra, 
+//      calcular o valor total do estoque.
+template <typename T> //T é o tipo de vinho(CLASSE) .
+class Vinoteca{
+    protected:
+        vector<T> lista_de_vinhos; //T é o tipo de vinho(OBJETO) .
+
+    public:
+        void adicionar_vinho(T vv){lista_de_vinhos.push_back(vv);} //T é o tipo de vinho(OBJETO) .
+        void listar_vinhos(){
+            for (int i = 0; i < lista_de_vinhos.size(); i++){
+                lista_de_vinhos[i].exibir_informacoes();
+            }
+        }
+
+        void encontrar_vinho_por_safra(int ss){
+            for (int i = 0; i < lista_de_vinhos.size(); i++){
+                if (lista_de_vinhos[i].get_safra() == ss){
+                    lista_de_vinhos[i].exibir_informacoes();
+                }
+            }
+        }
+
+        double calcular_valor_total_do_estoque(){
+            double soma = 0;
+            for (int i = 0; i < lista_de_vinhos.size(); i++){
+                soma += lista_de_vinhos[i].get_quantidade() * lista_de_vinhos[i].get_volume();
+            }
+            return soma;
+        }
+};
+
+int main() {
+    // Crie uma instância da Vinicola (singleton)
+    Vinicola *vinicola = Vinicola::get_instance();
+
+    // Adicione algumas uvas
+    Uva uva1("Uva A", 2020, 5.0);
+    Uva uva2("Uva B", 2021, 6.0);
+    Uva uva3("Uva C", 2019, 4.5);
+    vinicola->adicionar_uva(uva1);
+    vinicola->adicionar_uva(uva2);
+    vinicola->adicionar_uva(uva3);
+
+    // Crie alguns vinhos
+    VinhoTinto vinhoTinto1("Vinho Tinto A", 2020, 0.75, &uva1, 10, 3.5, 200);
+    VinhoBranco vinhoBranco1("Vinho Branco A", 2021, 0.75, &uva2, 15, "Seco");
+    VinhoTinto vinhoTinto2("Vinho Tinto B", 2021, 0.75, &uva2, 8, 4.0, 220);
+    VinhoBranco vinhoBranco2("Vinho Branco B", 2019, 0.75, &uva3, 12, "Suave");
+    vinicola->adicionar_vinho(vinhoTinto1);
+    vinicola->adicionar_vinho(vinhoBranco1);
+    vinicola->adicionar_vinho(vinhoTinto2);
+    vinicola->adicionar_vinho(vinhoBranco2);
+
+    // Crie alguns clientes
+    Client cliente1("Cliente 1", "Sobrenome 1", "cliente1@email.com", "123456789");
+    Client cliente2("Cliente 2", "Sobrenome 2", "cliente2@email.com", "987654321");
+    vinicola->adicionar_cliente(cliente1);
+    vinicola->adicionar_cliente(cliente2);
+
+    // Realize vendas
+    Venda venda1(&cliente1, &vinhoTinto1, 3);
+    Venda venda2(&cliente2, &vinhoBranco1, 5);
+    vinicola->registrar_venda(venda1);
+    vinicola->registrar_venda(venda2);
+
+    // Crie duas Vinotecas: uma para vinhos tintos e outra para vinhos brancos
+    Vinoteca<VinhoTinto> vinotecaTintos;
+    Vinoteca<VinhoBranco> vinotecaBrancos;
+
+    // Adicione alguns vinhos às Vinotecas
+    vinotecaTintos.adicionar_vinho(vinhoTinto1);
+    vinotecaTintos.adicionar_vinho(vinhoTinto2);
+    vinotecaBrancos.adicionar_vinho(vinhoBranco1);
+    vinotecaBrancos.adicionar_vinho(vinhoBranco2);
+
+    // Realize buscas nas Vinotecas por safra
+    int safraParaBuscar = 2021;
+    cout << "Vinhos tintos da safra " << safraParaBuscar << ":" << endl;
+    vinotecaTintos.encontrar_vinho_por_safra(safraParaBuscar);
+
+    cout << "Vinhos brancos da safra " << safraParaBuscar << ":" << endl;
+    vinotecaBrancos.encontrar_vinho_por_safra(safraParaBuscar);
+
+    // Calcule e exiba o valor total das Vinotecas
+    double valorTotalTintos = vinotecaTintos.calcular_valor_total_do_estoque();
+    double valorTotalBrancos = vinotecaBrancos.calcular_valor_total_do_estoque();
+
+    cout << "Valor total dos vinhos tintos na Vinoteca: " << valorTotalTintos << " unidades monetárias." << endl;
+    cout << "Valor total dos vinhos brancos na Vinoteca: " << valorTotalBrancos << " unidades monetárias." << endl;
+
+    return 0;
 }
